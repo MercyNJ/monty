18870b31be07efcf64023e97a0040510972218e6
MercyNJ <mercinjuguna@gmail.com>
2023-05-24 06:40:20 -0700

    Betty styling compliance check

--- add.c
+++ add.c
@@ -17,16 +17,16 @@ void add(stack_t **stack, unsigned int line_number)
 	stack_t *top1;
 	stack_t *top2;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    top1 = *stack;
-    top2 = (*stack)->next;
+	top1 = *stack;
+	top2 = (*stack)->next;
 
-    top2->n += top1->n;
+	top2->n += top1->n;
 
-    pop(stack, line_number);
+	pop(stack, line_number);
 }
